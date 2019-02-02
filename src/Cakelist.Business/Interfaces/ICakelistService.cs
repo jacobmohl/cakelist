89a965f6b4003a891e2f3e52a94b88220cd24d9f
Jacob Møhl <jamo@semler.dk>
2019-02-02 11:31:49 +0100

    Add basic Cakelist Service

---
+++ src/Cakelist.Business/Interfaces/ICakelistService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cakelist.Business.Interfaces
+{
+    interface ICakelistService
+    {
+        void GetCakelist();
+        void AddCakeRequest();
+        void VoteOnCakeRequest();
+    }
+}
