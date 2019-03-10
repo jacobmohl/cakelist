fb20a1000ed880829e88ba1811cc6924b19f4552
Jacob Møhl <jamo@semler.dk>
2019-03-10 11:29:16 +0100

    Add Application Insights to VS projects.

--- src/Cakelist.Web/Program.cs
+++ src/Cakelist.Web/Program.cs
@@ -19,6 +19,7 @@ namespace Cakelist.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
