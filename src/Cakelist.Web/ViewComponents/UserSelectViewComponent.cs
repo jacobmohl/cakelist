65fc36483f7c41d3e88e6371dc26ffe36c10ee2c
Jacob Møhl <jamo@semler.dk>
2019-05-31 21:57:44 +0200

    Added UserListViewComponent

---
+++ src/Cakelist.Web/ViewComponents/UserSelectViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cakelist.Business.Entities;
+using Cakelist.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cakelist.Web.ViewComponents
+{
+    public class UserSelectViewComponent : ViewComponent
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserSelectViewComponent(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string selectFor)
+        {
+            var users = _userRepository.ListAllAsync();
+            var userSelectListItems = SelectListItem(await users);
+
+            var viewModel = new UserSelectViewModel {
+                Users = userSelectListItems,
+                SelectFor = selectFor
+            };
+
+            return View(viewModel);
+        }
+
+        private static IEnumerable<SelectListItem> SelectListItem(IEnumerable<User> users)
+        {
+            return users.Select(u => new SelectListItem(u.FullName(), u.Id.ToString()));
+        }
+    }
+
+    public class UserSelectViewModel
+    {
+        public IEnumerable<SelectListItem> Users { get; set; }
+        public string SelectFor { get; set; }
+    }
+}
